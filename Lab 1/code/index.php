b91fcbbcff8a7f38a2b409c92c76b56f09e5c327
DeDUnitY <76110548+DeDUnitY@users.noreply.github.com>
2024-03-09 19:21:14 +0200

    lab 2 task 1

--- Lab 1/code/index.php
+++ Lab 1/code/index.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+// task 1
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = & $very_bad_unclear_name;
+$order .= ", but have some new letters";
+
+echo "\nYour order is: $very_bad_unclear_name.\n";
